a00fc53a60eab5427ad203a228001ba5c2decdfc
Roland Sauter <sauter.roland@pm.me>
2024-05-18 14:00:22 +0200

    minor typography fixes

--- code_images/compose_supplements.py
+++ code_images/compose_supplements.py
@@ -27,15 +27,15 @@ fig = sg.compose.Figure(
     *('160', '105'),
     sg.compose.Panel(
         atp.scale(scale).move(offset_x, offset_y),
-        sg.compose.Text('a)', 0, 0, size=text_size)
+        sg.compose.Text('(a)', 0, 0, size=text_size)
     ).move(x1, y1), 
     sg.compose.Panel(
         ldh.scale(scale).move(offset_x, offset_y),
-        sg.compose.Text('b)', 0, 0, size=text_size)
+        sg.compose.Text('(b)', 0, 0, size=text_size)
     ).move(x2, y1),
     sg.compose.Panel(
         c5.scale(scale).move(offset_x, offset_y),
-        sg.compose.Text('c)', 0, 0, size=text_size)
+        sg.compose.Text('(c)', 0, 0, size=text_size)
     ).move(x1, y2),
 )
 fig.save(supplement_path / 'figure_s5.svg')
@@ -45,11 +45,11 @@ fig.save(supplement_path / 'figure_s5.svg')
 atp = sg.compose.SVG(supplement_path / 'atp_flux.svg')
 n_rxns = sg.compose.SVG(supplement_path / 'number_reactions.svg')
 
-x1 = 2
-x2 = 90
+x1 = 5
+x2 = 95
 y1 = 2
 y2 = 57
-offset_x = -1
+offset_x = -4
 offset_y = 5
 
 scale1 = .19
@@ -57,14 +57,14 @@ scale2 = .2
 text_size = 5.
 
 fig = sg.compose.Figure(
-    *('180', '70'),
+    *('185', '70'),
     sg.compose.Panel(
         atp.scale(scale1).move(0,0),
-        sg.compose.Text('a)', offset_x, offset_y, size=text_size)
+        sg.compose.Text('(a)', offset_x, offset_y, size=text_size)
     ).move(x1, y1),
     sg.compose.Panel(
         n_rxns.scale(scale1).move(0, 0),
-        sg.compose.Text('b)', offset_x, offset_y, size=text_size)
+        sg.compose.Text('(b)', offset_x, offset_y, size=text_size)
     ).move(x2, y1),
 )
 fig.save(supplement_path / "figure_s3.svg")
@@ -86,11 +86,11 @@ fig = sg.compose.Figure(
     *('180', '65'),
     sg.compose.Panel(
         b.scale(scale).move(0,0),
-        sg.compose.Text('a)', offset_x, offset_y, size=text_size)
+        sg.compose.Text('(a)', offset_x, offset_y, size=text_size)
     ).move(x1, y1), 
     sg.compose.Panel(
         s.scale(scale).move(0,0),
-        sg.compose.Text('b)', offset_x, offset_y, size=text_size)
+        sg.compose.Text('(b)', offset_x, offset_y, size=text_size)
     ).move(x2, y1),
 )
 fig.save(supplement_path / "figure_s6.svg")
